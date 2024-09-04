922fd99db1926f6adef48b8a674ea4fcf93afc05
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:24:42 -0400

    Argsss

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+P1=$1
+P2=$2
+
+
+echo "$P1: $P2,bdjbdgjdgm,$P1"
+echo "$P2: $P2,hfjbmeksckns,$P1"
+echo "$P1: $P1,bdjbdgjdegfhfbgm"
+echo "$P2: $P1,hfjbmeksfhhfcknsdbddb"
+echo "$P1: $P2,bdjbdgffvbxvvgbffjdgm,$P1,"
+echo "$P2: $P2,hfjbmekbffbcbscknsfjdgkdngkdfgdhdgnk.,$P1"
