e036386c598f884c04e75ecee2816ca82e578584
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:02:38 -0400

    1

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "bdjbdgjdgm"
-echo "hfjbmeksckns"
+echo "Hem: Suneel,bdjbdgjdgm,Hem"
+echo "Suneel: Suneel,hfjbmeksckns,Hem"
+echo "Hem: Hem,bdjbdgjdegfhfbgm"
+echo "Suneel: Hem,hfjbmekscknsdbddb"
+echo "Hem: Suneel,bdjbdgfvgbffjdgmHem,"
+echo "Suneel: Suneel,hfjbmekscknsfjdgkdngkdgnk.,Hem"
