78a5a5107985d5cf556a1f2850f367bda3f3e2ca
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:50:36 -0400

    Updated files

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# index is starts from 0, size is 3
+Fruit=("Apple" "Orange" "kiwi") #array
+
+
+echo "first fruit is: ${Fruit[0]}"
+echo "Second fruit is: ${Fruit[1]}"
+echo "Third fruit is: ${Fruit[2]}"
+
+echo "first fruit is: ${Fruit[@]}"
