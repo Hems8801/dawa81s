46ac9d8e8a32a9f59ad3ac20b7f7a326c3dc89e4
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:33:48 -0400

    Read-s

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -5,8 +5,13 @@ P2=$2
 
 echo "Enter your USERNAME"
 
-read USERNAME
+read -s USERNAME
 echo "Entered USERNAME is: $USERNAME"
+
+echo "Enter your Password"
+
+read -s PASSWORD
+echo "Entered USERNAME is: $PASSWORD
 
 #echo "$P1: $P1,bdjbdgjdegfhfbgm"
 #echo "$P2: $P1,hfjbmeksfhhfcknsdbddb"
