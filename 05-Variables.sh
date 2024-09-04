628070a4f7be6dfb0301ca6cd44a07315184929c
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:34:52 -0400

    Read -s to hide the uname and password while entering

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -16,4 +16,4 @@ echo "Entered USERNAME is: $PASSWORD
 #echo "$P1: $P1,bdjbdgjdegfhfbgm"
 #echo "$P2: $P1,hfjbmeksfhhfcknsdbddb"
 #echo "$P1: $P2,bdjbdgffvbxvvgbffjdgm,$P1,"
-#echo "$P2: $P2,hfjbmekbffbcbscknsfjdgkdngkdfgdhdgnk.,$P1"
+#echo "$P2: $P2,hfjbmekbffbmxc mxvnv, vdcbscknsfjdgkdngkdfgdhdgnk.,$P1"
