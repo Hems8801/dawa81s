88e2f48fa82cecebcd135ec56bb4863330391eea
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:12:46 -0400

    02

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-P1=Arif
-P2=suresh
-
-
-echo "$P1: $P2,bdjbdgjdgm,$P1"
-echo "$P2: $P2,hfjbmeksckns,$P1"
-echo "$P1: $P1,bdjbdgjdegfhfbgm"
-echo "$P2: $P1,hfjbmekscknsdbddb"
-echo "$P1: $P2,bdjbdgfvgbffjdgm,$P1,"
-echo "$P2: $P2,hfjbmekscknsfjdgkdngkdgnk.,$P1"
+echo "Hem: Suneel,bdjbdgjdgm,Hem"
+echo "Suneel: Suneel,hfjbmeksckns,Hem"
+echo "Hem: Hem,bdjbdgjdegfhfbgm"
+echo "Suneel: Hem,hfjbmekscknsdbddb"
+echo "Hem: Suneel,bdjbdgfvgbffjdgmHem,"
+echo "Suneel: Suneel,hfjbmekscknsfjdgkdngkdgggnk.,Hem"
