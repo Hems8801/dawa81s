fc2488629c1f9e2b797ce2efc304a4b118a3222c
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 02:30:28 -0400

    Read

---
+++ 05-Variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+P1=$1
+P2=$2
+
+
+echo "Enter your USERNAME"
+
+read USERNAME
+echo "Entered USERNAME is: $USERNAME"
+
+#echo "$P1: $P1,bdjbdgjdegfhfbgm"
+#echo "$P2: $P1,hfjbmeksfhhfcknsdbddb"
+#echo "$P1: $P2,bdjbdgffvbxvvgbffjdgm,$P1,"
+#echo "$P2: $P2,hfjbmekbffbcbscknsfjdgkdngkdfgdhdgnk.,$P1"
