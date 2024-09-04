c2db819e4dbd1f1c5ca7e2b4709b7f8c3ab436ac
Hems8801 <hemanthlebaka@gmail.com>
2024-09-04 13:33:48 -0400

    Special Variable

--- 09-Specialvars.sh
+++ 09-Specialvars.sh
@@ -1,3 +1,10 @@
 #!/bim/bash
 
 echo "All Variables Passed to the script : $@"
+echo "Number of variables passed : $#"
+echo "Script name : $0"
+echo "Current Working Directory : $PWD"
+echo "Home Directory : $HOME"
+echo "PID of the script executing now : $$"
+sleep 100 &
+echo "PID of last background command executed : $!"
